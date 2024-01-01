0a2aa85aa97558cc8c6c6f12711d1c2c58616cb6
unknown <Reetika44>
2024-01-01 12:10:51 +0530

    laying-foundation episode-3

--- App.js
+++ App.js
@@ -1,53 +1,51 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
 
-// React Elements are equivalent to DOM Elements(HTML Elements). React Element is not HTML Element. It is object.
-// React.createElement => ReactElement(JS object) => when we render this on to the DOM, It becomes HTMLElement(render).
-
-// const heading = React.createElement("h1",{id:"heading"},"reetika");
-
-// console.log(heading)
-
-// JSX (transpiled before it reaches js engine) - PARCEL - Babel(normal js package) JSX convert it to code what react,js engine understands
-// JSX => Babel transpiles it to React.createElement => ReactElement(JS object) => when we render this on to the DOM, It becomes HTMLElement(render).
-
-// If JSX is in single line, this is good
-// If JSX in multiple lines, you have to wrap around --round () brackets -- Mandatory
-const heading = (<h1 className="head" tabIndex="1">Namaste React using JSX</h1>)
-
-console.log(heading)
-
-// jsxHeading is react element(object)
+const elem = <span>
+  React Element
+</span>
 
 
-// React Components
-
-//   Two Types of component
-//     A. Class based Components - OLD - JS Classes
-//     B. Functional Components - NEW  - JS Functions
-
-
-//  React Functional Component is normal JS Function which returns some piece of JSX Element/Code or A funtion which returns react element
-//  Name it with capital letter
+const TitleNew = function (){
+ return (
+    <h1 className="head" tabIndex="1">
+      Namaste React using JSX
+    </h1>
+  )
+}
 const HeadingComponent = () => <h1 className="heading">Namaste React Functional Component</h1>;
-// or both are same
 
-// Akshay saini prefers with return
+const number = 1000;
+
+const Title = () =>
+  (<h1 className="head" tabIndex="1">
+    {elem}
+    Namaste React using JSX
+  </h1>
+);
+
+// const data = api.getData();
+//suppose api sends some malicious data -- cross site scripting -- read local storage, cookies, read data stc... if i can run js in your browser or computer. JSX takes care of these injection attacks jsx will escape thing, it will sanitize the data, when the data is in {}. It prevents injection attacks.
+
+// How can i put react element(title) inside component
 const HeadingComponent1 = () => (
  <div id="container">
-   <h1 className="heading"s>Namaste React Functional Component</h1>;
+   
+   {
+    // Any piece of JS Code
+    // can call JS function
+    // Title()
+    1 + 2
+    }
+    <Title />
+    {/* {data} */}
+   
+   <h1 className="heading"s>Namaste React Functional Component</h1>
  </div>
 );
 
-const fn = () => true;  // function is returning true - short hand Both functions are same (arrow functions)
-const fn1= ()=>{
-    return true;
-}
 
+  
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(heading);
-
-// It is not developer friendly(tough)or not readable, therefor JSX Comes.
-// JSX is not part of React. JSX is not HTML inside JS. JSX is HTML-like or XML-like syntax.
-// We can write React without JSX also. JSX makes developer life easy.It is an extension where we can merge HtML and JS.
+root.render(<HeadingComponent1 />);
